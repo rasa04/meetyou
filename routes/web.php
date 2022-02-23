f86a84d2f936cf647a181b33f004098ef1e853fd
Rasa <abdurasul2004.02@gmail.com>
2022-02-24 00:38:21 +0500

    beta 0

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/welcome', 'welcome', ['name'=>'Rasa', 'greeting'=>'Привет!']);
+
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+Route::any('/any', function () {
+    return 'any';
+});
+Route::redirect('/here', '/there');
+
+Route::get('/user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return "Post: {$postId}" . "<hr>Comment: {$commentId}";
+});
+
+use Illuminate\Http\Request;
+ 
+Route::get('/superuser/{id}', function (Request $request, $id) {
+    return 'User '.$id;
+});
